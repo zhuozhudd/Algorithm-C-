fa0948432f5aa9ac1719bc05fc19b92d637429ee
WallE <57135399+zhuozhudd@users.noreply.github.com>
2022-05-02 10:15:19 +0800

    main

--- UnionFind/main.cpp
+++ UnionFind/main.cpp
@@ -1,3 +1,8 @@
+/*
+ * @Author: szz
+ * @Date: 2022-05-02 10:00:05
+ * @Description: 
+ */
 #include <iostream>
 #include "TestUnionFind.h"
 #include "UnionFindTestHelper.h"
@@ -6,8 +11,8 @@ int main() {
     int n = 1000000;
     // UnionFindTestHelper::testUF1(n);
     // UnionFindTestHelper::testUF2(n);
-    UnionFindTestHelper::testUF3(n); // size优化
-    UnionFindTestHelper::testUF4(n); // rank优化
+    // UnionFindTestHelper::testUF3(n); // size优化
+    // UnionFindTestHelper::testUF4(n); // rank优化
     UnionFindTestHelper::testUF5(n); // 路径压缩
     return 0;
 }
